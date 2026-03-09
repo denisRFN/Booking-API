ac239b0cf4a835d65226ab59a1adca7c28454a43
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-03-09 10:34:50 +0200

    Update auth.py

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,15 +1,24 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.schemas.user import UserCreate
+
+from app.schemas.user import UserCreate, UserLogin
 from app.models.user import User
 from app.db.session import get_db
-from app.core.security import hash_password
+
+from app.core.security import hash_password, verify_password
+from app.core.auth import create_token
+
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/register")
 def register(user: UserCreate, db: Session = Depends(get_db)):
+
+    # verificam daca userul exista deja
+    existing_user = db.query(User).filter(User.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
 
     new_user = User(
         username=user.username,
@@ -22,4 +31,32 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
 
-    return new_user
+    return {
+        "id": new_user.id,
+        "username": new_user.username,
+        "email": new_user.email,
+        "role": new_user.role
+    }
+
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.username == user.username).first()
+
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if not verify_password(user.password, db_user.password_hash):
+        raise HTTPException(status_code=401, detail="Invalid password")
+
+    token = create_token({
+        "user_id": db_user.id,
+        "role": db_user.role
+    })
+
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "role": db_user.role
+    }
