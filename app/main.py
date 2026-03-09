6f4ba9b9267b55cb7e31c1f0f494c47b860c5a1e
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-03-09 10:35:27 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,9 +3,12 @@ from app.api import reservations
 from app.api import desks
 from app.models.desk import Base
 from app.db.database import engine
+from app.api import auth
+
 
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 app.include_router(desks.router)
 app.include_router(reservations.router)
+app.include_router(auth.router)
