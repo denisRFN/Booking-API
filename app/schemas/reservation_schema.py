0520a94634e9a4ba2c8e2cb2e8193745eca14795
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-03-09 09:47:18 +0200

    Create reservation_schema.py

---
+++ app/schemas/reservation_schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import date, time
+from uuid import UUID
+
+
+class ReservationCreate(BaseModel):
+
+    desk_id: UUID
+    user_id: UUID
+
+    date: date
+
+    start_time: time
+    end_time: time
