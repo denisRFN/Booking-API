528cf89f48ea8a09e90876cafc7f026ad1739430
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-03-09 10:11:46 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
 
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost/desk_booking"
+    DATABASE_URL: str
 
     class Config:
         env_file = ".env"
