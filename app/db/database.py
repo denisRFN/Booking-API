6aaed924ba718544aa397da17c6d6f81715e0952
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-03-09 10:46:10 +0200

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
@@ -10,3 +10,5 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
