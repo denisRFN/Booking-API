f8325be92aa073565ed4e555b7c48aa119911202
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-03-09 09:55:26 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.api import reservations
 from app.api import desks
+from app.models.desk import Base
+from app.db.database import engine
+
 
 app = FastAPI()
-
+Base.metadata.create_all(bind=engine)
 app.include_router(desks.router)
 app.include_router(reservations.router)
