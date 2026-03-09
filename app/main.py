b22ef8fc56ac5c5ba6e870535dcb223c67ca68f4
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-03-09 09:54:59 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api import reservations
+from app.api import desks
 
 app = FastAPI()
 
+app.include_router(desks.router)
 app.include_router(reservations.router)
